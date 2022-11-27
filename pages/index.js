0edac2af02889b08d58c934aac28bcbdf7265283
Chacs <chacko@skykatltd.com>
2022-11-27 00:11:26 +0000

    getserversideprops

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import { useState } from "react"
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from "next/link"
+import { useRouter } from "next/router"
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [query, getQuery] = useState();
+  const router = useRouter()
+  const handleOnChange = e => getQuery(e.target.value)
+  const handleOnSubmit = e => {
+    e.preventDefault()
+    router.push(`/news/${query}`)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +24,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>News Feed</h1>
+
+        <form onSubmit={handleOnSubmit}>
+          <input type="text" onChange={handleOnChange} />
+        </form>
 
         <div className={styles.grid}>
           <Link href="/news/">
@@ -31,12 +45,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,40 +1,30 @@
-import Layout from "../../components/Layout";
+import Layout from "../../components/Layout"
+import { handler } from "../api"
 
 export default function News({ results }) {
-  return (
-    <Layout>
-      <h1>Top Stories</h1>
-      <ul>
-        {results.map((result) => {
-          return (
-            <li key={result.uri}>
-              <a
-                href={result.url}
-                target="_blank"
-                rel="noopener norefferer noreferrer"
-              >
-                {result.title}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </Layout>
-  );
+    return(
+      <Layout>
+        <h1>Top Stories</h1>
+        <ul>
+            {results.map(result => {
+              return(<li key={result.uri}><a href={result.url} target="_blank" rel="noopener norefferer">{result.title}</a></li>)
+            })}
+        </ul>
+      </Layout>
+    )
 }
 
-const API_KEY = "api key";
-
+// to register for a new New York Times API KEY, visit : 
+const API_KEY = "9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ"
 export async function getStaticProps() {
-  const URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`;
-  const response = await fetch(URL);
-  const data = await response.json();
-  console.log(data.results);
+
+  const results = await handler(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`
+  )
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props: {
-      results: data.results,
-    },
-  };
+      results
+    }
+  }
 }
