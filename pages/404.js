d7228e47717d289ed0b5ed4a0a3383337a7835cc
Chacs <chacko@skykatltd.com>
2022-11-27 11:37:55 +0000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,37 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import gif from "../public/not-found.gif";
 import Layout from "../components/Layout";
+
 export default function NotFound() {
+  const router = useRouter();
+  const goBack = () => {
+    router.back();
+  };
   return (
     <Layout>
-      <h1>Not Found</h1>
+      <br />
+      <a href="#" onClick={goBack}>
+        Back
+      </a>
+      <div className="container">
+        <h1>Sorry we could not find what you are looking for</h1>
+        <Image src={gif} alt="not found" />
+        <style jsx>
+          {`
+            .container {
+              height: 100vh;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-direction: column;
+            }
+            h1 {
+              color: chocolate;
+            }
+          `}
+        </style>
+      </div>
     </Layout>
   );
 }
